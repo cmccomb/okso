8033c1fe9038c06e58a93c9ceb90b52f81e4f7cf
Chris McComb <ccmcc2012@gmail.com>
2026-01-17 10:01:06 -0500

    rename *.bats to *.sh

--- src/lib/planning/planner.sh
+++ src/lib/planning/planner.sh
@@ -351,7 +351,7 @@ generate_planner_response() {
 
 	# Configure sampling parameters
 	local sample_count temperature debug_log_dir debug_log_file max_generation_tokens
-	sample_count="$(validate_positive_int "${PLANNER_SAMPLE_COUNT:-3}" 3 "PLANNER_SAMPLE_COUNT")"
+	sample_count=1 #"$(validate_positive_int "${PLANNER_SAMPLE_COUNT:-3}" 3 "PLANNER_SAMPLE_COUNT")"
 	temperature="$(validate_temperature "${PLANNER_TEMPERATURE:-0.7}" 0.7)"
 	max_generation_tokens="$(validate_positive_int "${PLANNER_MAX_OUTPUT_TOKENS:-1024}" 1024 "PLANNER_MAX_OUTPUT_TOKENS")"
 
