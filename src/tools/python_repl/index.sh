233bead1263d9faa0385d16ff895f067928fafa5
Chris McComb <ccmcc2012@gmail.com>
2026-01-17 11:34:33 -0500

    intensified

--- src/tools/python_repl/index.sh
+++ src/tools/python_repl/index.sh
@@ -289,7 +289,7 @@ register_python_repl() {
 	args_schema=$(jq -nc --arg key "$(canonical_text_arg_key)" '{"type":"object","required":[$key],"properties":{($key):{"type":"string","minLength":1}}}')
 	register_tool \
 		"python_repl" \
-		"Execute Python statements in a temporary sandbox. Use print statements to view values." \
+		"Execute Python statements in a temporary sandbox. However, you MUST use print statements to view outputs." \
 		"Writes are confined to an ephemeral sandbox directory." \
 		tool_python_repl \
 		"${args_schema}"
